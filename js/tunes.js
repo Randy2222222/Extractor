ee0783fa606d02a79a725654128ab63b19a3f99f
Randy2222222 <randyjpollitt@yahoo.com>
2026-01-09 19:18:37 -0500

    Refactor audio controls and add song playlist

--- js/tunes.js
+++ js/tunes.js
@@ -1,17 +1,54 @@
-const audio = document.getElementById("audio");
-const playPauseButton = document.getElementById("play-pause-button");
-const volumeControl = document.getElementById("volume-control");
+const audio = document.getElementById('audio');
+const playButton = document.getElementById('play');
+const pauseButton = document.getElementById('pause');
+const nextButton = document.getElementById('next');
+const previousButton = document.getElementById('previous');
+const volumeControl = document.getElementById('volume');
+const trackName = document.getElementById('track-name');
+const trackArtist = document.getElementById('track-artist');
+const playlistElement = document.getElementById('playlist');
 
-playPauseButton.addEventListener("click", function() {
-    if (audio.paused) {
-        audio.play();
-        playPauseButton.textContent = "Pause";
-    } else {
-        audio.pause();
-        playPauseButton.textContent = "Play";
-    }
+const songs = [
+    { title: "Song 1", artist: "Artist 1", src: "song1.mp3" },
+    { title: "Song 2", artist: "Artist 2", src: "song2.mp3" },
+    { title: "Song 3", artist: "Artist 3", src: "song3.mp3" }
+];
+
+let currentSongIndex = 0;
+
+function loadSong(song) {
+    audio.src = song.src;
+    trackName.textContent = song.title;
+    trackArtist.textContent = song.artist;
+}
+
+function playSong() {
+    audio.play();
+}
+
+function pauseSong() {
+    audio.pause();
+}
+
+function nextSong() {
+    currentSongIndex = (currentSongIndex + 1) % songs.length;
+    loadSong(songs[currentSongIndex]);
+    playSong();
+}
+
+function previousSong() {
+    currentSongIndex = (currentSongIndex - 1 + songs.length) % songs.length;
+    loadSong(songs[currentSongIndex]);
+    playSong();
+}
+
+playButton.addEventListener('click', playSong);
+pauseButton.addEventListener('click', pauseSong);
+nextButton.addEventListener('click', nextSong);
+previousButton.addEventListener('click', previousSong);
+volumeControl.addEventListener('input', (e) => {
+    audio.volume = e.target.value;
 });
 
-volumeControl.addEventListener("input", function() {
-    audio.volume = volumeControl.value;
-});
+// Load the first song
+loadSong(songs[currentSongIndex]);
